a497183a1e27823786a15175f4731d470637272c
Exerchan <58511942+Exerchan@users.noreply.github.com>
2020-03-31 16:11:07 +0200

    Added playermovement and cameracontrol
    Thirdperson view added with movement.
    Added two buildings

---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 50f;
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
