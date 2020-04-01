ec335f4b0cea60b3315b348e400b9d28578be1c1
rick2909 <rick29998@gmail.com>
2020-04-01 11:58:11 +0200

    Add wall running

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -7,10 +7,21 @@ public class MouseLook : MonoBehaviour {
     float xRotation = 0f;
 
     public Transform body;
+    public GameObject player;
+    PlayerMovement playerMovement;
+
+    public float maxTilt = 30f;
+    public float leanSpeed = 5f;
+    public float leanBackSpeed = 6f;
+    float tilt = 0;
+
+
 
     // Start is called before the first frame update
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
+
+        playerMovement = player.GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -18,10 +29,65 @@ public class MouseLook : MonoBehaviour {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        float z = 0;
+
+        if (playerMovement.isWallrunningRight && !playerMovement.isGrounded) {
+            z = LeanRight();
+        }else if (playerMovement.isWallrunningLeft && !playerMovement.isGrounded) {
+            z = LeanLeft();
+        } else {
+            z = LeanBack();
+        }
+
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, z);
         body.Rotate(Vector3.up * mouseX);
+    }
+
+    private float LeanLeft()
+    {
+        float currAngle = transform.rotation.eulerAngles.z;
+
+        float targetAngle = maxTilt - 360;
+
+        if (currAngle > 180.0) {
+            targetAngle = 360.0f - maxTilt;
+        }
+
+        float angle = Mathf.Lerp(currAngle, targetAngle, leanSpeed * Time.deltaTime);
+
+        return angle;
+    }
+
+    private float LeanRight()
+    {
+        float currAngle = transform.rotation.eulerAngles.z;
+
+        float targetAngle = maxTilt;
+
+
+        if (currAngle > 180.0) {
+            currAngle = 360 - currAngle;
+        }
+
+        float angle = Mathf.Lerp(currAngle, targetAngle, leanSpeed * Time.deltaTime);
+
+        return angle;
+    }
+
+    private float LeanBack() {
+        float currAngle = transform.rotation.eulerAngles.z;
+
+        float targetAngle = 0f;
+
+        if(currAngle > 180) {
+            targetAngle = 360;
+        }
+
+        float angle = Mathf.Lerp(currAngle, targetAngle, leanSpeed * Time.deltaTime);
+
+        return angle;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,36 +8,89 @@ public class PlayerMovement : MonoBehaviour {
 
     public float speed = 12f;
     public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    public float shortJumpHeight = 2.5f;
+    public float longJumpHeight = 3.1f;
+    public float sprintSpeed = 1.5f;
+
+    public float delay = 0.5f;
+    float startTime;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundmask;
+    public bool isGrounded;
 
-    bool isGrounded;
+    public Transform wallCheckLeft;
+    public Transform wallCheckRight;
+    public float wallDistance = 0.4f;
+    public float wallGravity = -1.5f;
+    public LayerMask Wallmask;
+    public bool isWallrunningLeft;
+    public bool isWallrunningRight;
+
+    bool jumpOnce = false;
 
     Vector3 velocity;
 
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update() {
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundmask);
+        isWallrunningLeft = Physics.CheckSphere(wallCheckLeft.position, wallDistance, Wallmask);
+        isWallrunningRight = Physics.CheckSphere(wallCheckRight.position, wallDistance, Wallmask);
+
 
         if (isGrounded && velocity.y < 0) {
             velocity.y = -2f;
         }
-        
+
+        if(!isGrounded && isWallrunningLeft && velocity.y < 0) {
+            velocity.y = wallGravity;
+        } else if(!isGrounded && isWallrunningRight && velocity.y < 0) {
+            velocity.y = wallGravity;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+
+        if (Input.GetButton("Running")) {
+            z *= sprintSpeed;
+        }
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded) {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        if (Input.GetButtonDown("Jump")) {
+            startTime = Time.time;
         }
+
+        Debug.Log("Before if: " + Time.time + " "+ jumpOnce);
+
+        if (!jumpOnce && (isGrounded || isWallrunningLeft || isWallrunningRight)) {
+            if (Input.GetButtonUp("Jump") && Time.time - startTime < delay) {
+                Debug.Log("Short jump");
+                velocity.y = Mathf.Sqrt(shortJumpHeight * -2f * gravity);
+                jumpOnce = true;
+            }
+            else if (Input.GetButton("Jump") && Time.time - startTime > delay) {
+                Debug.Log("Long jump");
+                velocity.y = Mathf.Sqrt(longJumpHeight * -2f * gravity);
+                jumpOnce = true;
+            }
+        }
+        else if(!isGrounded && !isWallrunningLeft && !isWallrunningRight){
+            jumpOnce = false;
+        }
+
+        Debug.Log("after if: " + Time.time + " " + jumpOnce);
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-    }
+    } 
 }
